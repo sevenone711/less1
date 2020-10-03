c2969ccd2f39fa591bc172b6b4e9ab0846493921
sevenone711 <arbitronin@yandex.ru>
2020-10-03 14:25:23 +0300

    Игра угадай число!

--- src/lesson1.java
+++ src/lesson1.java
@@ -2,7 +2,7 @@ import com.sun.tools.javac.file.SymbolArchive;
 import org.omg.CORBA.PUBLIC_MEMBER;
 
 public class lesson1 {
-    public static void main(String args[]){
+    public static void main(String[] args){
         byte a = 1;
         short b = 30000;
         int c = 123123123;
