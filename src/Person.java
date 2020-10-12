6f1ebe64bacb85cc56f0db1ac74b048871871840
sevenone711 <arbitronin@yandex.ru>
2020-10-12 11:57:37 +0300

    Сделал всю домашку

---
+++ src/Person.java
@@ -0,0 +1,32 @@
+public class Person {
+    private String name;
+    private String position;
+    private String email;
+    private int age;
+    private String tel;
+    private int salary;
+
+
+    public Person (String name,String position, String email, int age,String tel, int salary){
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.age = age;
+        this.tel = tel;
+        this.salary = salary;
+    }
+
+     void info(){
+        System.out.println("Простой вывод \nИмя: " +this.name +"\nДолжность: " +this.position +"\n email: "+this.email +"" +
+                "\nВозраст: " + this.age +"\nТелефон: "+ this.tel +"\nЗарплата: "+ this.salary + " рублей\n");
+        System.out.println("----------------------------------");
+    }
+    void infos(){
+        if (this.age > 40){
+            System.out.println("Имя: " +this.name +"\nДолжность: " +this.position +"\n email: "+this.email +"" +
+                    "\nВозраст: " + this.age +"\nТелефон: "+ this.tel +"\nЗарплата: "+ this.salary + " рублей\n");
+        }
+    }
+
+
+}
